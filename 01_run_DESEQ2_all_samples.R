a1d6d40046f7be3a84c6b7c9a4ec9cd7a6f6f1de
trnguyenuka <trnguyenuka@github.com>
2024-11-27 15:02:33 +0100

    update

--- 01_run_DESEQ2_all_samples.R
+++ 01_run_DESEQ2_all_samples.R
@@ -12,6 +12,9 @@ PROJECT <- "ALaouina_241121"
   
 path.to.main.output <- file.path(outdir, PROJECT)
 dir.create(path.to.main.output, showWarnings = FALSE, recursive = TRUE)
+path.to.01.output <- file.path(path.to.main.output, "01_output")
+dir.create(path.to.01.output, showWarnings = FALSE, recursive = TRUE)
+
 source(file.path(path.to.main.src, "preparation.R"))
 source(file.path(path.to.main.src, "helper_functions_v2.R"))
 
@@ -21,10 +24,13 @@ meta.data <- readxl::read_excel(file.path(path.to.main.src, "Sample sheet Hieu.x
   mutate(condition = str_split(SampleName, "_")[[1]][[2]]) %>%
   mutate(age = str_split(SampleName, "_")[[1]][[3]]) %>%
   mutate(organ = ifelse(str_split(SampleName, "")[[1]][[1]] == "D", "distal", "proximal")) %>%
-  mutate(sample = str_split(FileName, "_R1")[[1]][[1]])
+  mutate(sample = str_split(FileName, "_R1")[[1]][[1]])%>%
+  mutate(full.condition = sprintf("%s_%s_%s", condition, age, organ))
 
+#####----------------------------------------------------------------#####
+##### RUN DESEQ2
+#####----------------------------------------------------------------#####
 salmon.output <- "/media/hieunguyen/HD01/outdir/CRC1382/241121_Laouina_Pabst_MolMedizin_3mRNAseq/star_salmon"
-
 path.to.tx2gene <- file.path(salmon.output, "tx2gene.tsv")
 deseq.dataset <- generate_DESeq2_dataset(path.to.salmon.quants = salmon.output,
                                          path.to.tx2gene = path.to.tx2gene,
@@ -33,3 +39,43 @@ tx2gene <- read_tsv(path.to.tx2gene,
                     col_names = c("transcript_id", "gene_id", "gene_name"), 
                     show_col_types = FALSE)
 deseq.output <- run_DESeq2_and_preprocess(deseq.dataset, tx2gene, thresh.pval = 0.05)
+
+#####----------------------------------------------------------------#####
+##### HEATMAP OF SELECTED GENES
+#####----------------------------------------------------------------#####
+countdf <- deseq.output$norm.count 
+gene.list <- c("Lct", "Lyz1", "Tff3", "Zg16", "Chga", "Olfm4", "Dclk1")
+
+countdf.filtered <- subset(countdf, gene_name %in% gene.list) %>%
+  subset(select = -c(gene_id)) 
+row.names(countdf.filtered) <- NULL
+heatmapdf <- countdf.filtered %>% column_to_rownames("gene_name") %>% t() %>% as.data.frame()
+
+heatmapdf.scaled <- (heatmapdf - rowMeans(heatmapdf))/rowSds(as.matrix(heatmapdf))
+heatmap.plot <- heatmapdf.scaled %>% rownames_to_column("SampleID") %>% 
+  pivot_longer(!SampleID, names_to = "signature", values_to = "z_score") %>%
+  ggplot(aes(y = SampleID, x = signature, fill = z_score)) + geom_tile() + 
+  scale_fill_distiller(palette = "RdBu") + 
+  theme(axis.text = element_text(size = 22))
+ggsave(plot = heatmap.plot, filename = "heatmap.svg", path = path.to.01.output, dpi = 300, width = 10, height = 10)
+
+#####----------------------------------------------------------------#####
+##### PCA plot
+#####----------------------------------------------------------------#####
+input.df <- deseq.output$norm.count[meta.data$sample]
+pca.object <- prcomp(t(input.df), rank. = 2, scale. = FALSE)
+
+pcadf <- data.frame(pca.object$x)
+
+row.names(pcadf) <- meta.data$sample
+
+pcadf <- merge(pcadf, meta.data, by.x = "row.names", by.y = "sample")
+
+pca.plot <- ggplot(pcadf, aes(x=PC1, y=PC2, color= condition, text = Row.names, label = Row.names)) +
+  geom_point(size = 4) +
+  theme_bw() + 
+  theme(plot.title = element_text(hjust=0.5, face="bold", size = 12)) +
+  geom_label_repel() + 
+  geom_hline(yintercept = 0) +
+  geom_vline(xintercept = 0)
+ggsave(plot = pca.plot, filename = "PCA.svg", path = path.to.01.output, dpi = 300, width = 10, height = 10)
--- helper_functions_v2.R
+++ helper_functions_v2.R
@@ -17,7 +17,7 @@ generate_DESeq2_dataset <- function(path.to.salmon.quants,
   txi <- tximport(files, type="salmon", tx2gene=tx2gene)
   output <- DESeqDataSetFromTximport( txi,
                                       colData = metadata,
-                                      design = ~ condition) # make sure that group exists in metadata
+                                      design = ~ full.condition) # make sure that group exists in metadata
   return(output)
 }
 
@@ -110,4 +110,106 @@ run_DESeq2_and_preprocess <- function(  deseq.dataset,
                  resultdf.nonsig = resultdf.nonsig,
                  deseq.object = deseq.object)
   return(output)
+}
+
+
+##### MAIN FUNCTION TO RUN DESEQ2 AND PLOTS
+
+run_pipeline_DESEQ2 <- function(salmon.output, 
+                                path.to.save.output,
+                                filtered.metadata){
+  ##### DESEQ2
+  path.to.tx2gene <- file.path(salmon.output, "tx2gene.tsv")
+  deseq.dataset <- generate_DESeq2_dataset(path.to.salmon.quants = salmon.output,
+                                           path.to.tx2gene = path.to.tx2gene,
+                                           metadata = filtered.metadata)
+  tx2gene <- read_tsv(path.to.tx2gene, 
+                      col_names = c("transcript_id", "gene_id", "gene_name"), 
+                      show_col_types = FALSE)
+  deseq.output <- run_DESeq2_and_preprocess(deseq.dataset, tx2gene, thresh.pval = 0.05)
+  
+  sigdf <- deseq.output$resultdf.sig
+  nonsigdf <- deseq.output$resultdf.nonsig
+  
+  writexl::write_xlsx(sigdf, file.path(path.to.save.output, "sig_DGE.xlsx"))
+  writexl::write_xlsx(nonsigdf, file.path(path.to.save.output, "nonsig_DGE.xlsx"))
+  
+  ##### PCA plot
+  input.df <- deseq.output$norm.count[filtered.metadata$sample]
+  pca.object <- prcomp(t(input.df), rank. = 2, scale. = FALSE)
+  
+  pcadf <- data.frame(pca.object$x)
+  
+  row.names(pcadf) <- filtered.metadata$sample
+  
+  pcadf <- merge(pcadf, filtered.metadata, by.x = "row.names", by.y = "sample")
+  
+  pca.plot <- ggplot(pcadf, aes(x=PC1, y=PC2, color= condition, text = Row.names, label = Row.names)) +
+    geom_point(size = 4) +
+    ggtitle(sprintf("PCA plot, Sample: %s vs. %s", condition1, condition2)) +
+    theme_bw() + 
+    theme(plot.title = element_text(hjust=0.5, face="bold", size = 12)) +
+    geom_label_repel() + 
+    geom_hline(yintercept = 0) +
+    geom_vline(xintercept = 0)
+  
+  ggsave(plot = pca.plot, filename = "PCA.svg", path = path.to.save.output, dpi = 300, width = 10, height = 10)
+  
+  ##### Volcano plot
+  cutoff.adjp <- 0.05
+  
+  input.df <- deseq.output$all.resultdf
+  input.df <- input.df %>% mutate(abs.log2FoldChange = abs(log2FoldChange))
+  input.df <- input.df %>% rowwise() %>%
+    mutate(show.gene.name = ifelse(padj < cutoff.adjp, gene_name, NA))
+  
+  volcano.plot <- ggplot(data=input.df, 
+                         aes(x=log2FoldChange, y=-log10(padj), col=sig, label=show.gene.name)) + 
+    geom_point() + 
+    scale_color_manual(values=c("#c0d2f0", "#f28095")) +
+    theme_minimal() +
+    geom_vline(xintercept=c(-1, 1), col="#9a9fa6", linetype='dotted') +
+    geom_hline(yintercept=-log10(cutoff.adjp), col="#9a9fa6", linetype='dotted') +
+    geom_text_repel() +
+    ggtitle(sprintf("Volcano plot, Sample: %s vs. %s", condition1, condition2)) +
+    theme_bw() + 
+    theme(plot.title = element_text(hjust=0.5, face="bold", size = 12)) +
+    xlim(c(-max(input.df$abs.log2FoldChange), max(input.df$abs.log2FoldChange))) +
+    ggtitle(sprintf("Positive LogFC: %s > %s (right-handed side)", 
+                        levels(filtered.metadata$full.condition)[[2]],
+                        levels(filtered.metadata$full.condition)[[1]]
+                        ))
+  ggsave(plot = volcano.plot, filename = "volcano_plot.svg", path = path.to.save.output, dpi = 300, width = 14, height = 10)
+  
+  ##### MA plot
+  ma.plot <- ggplot(data=input.df, 
+                    aes(x=log2(baseMean), y=log2FoldChange, col=sig, label=show.gene.name)) + 
+    geom_point() + 
+    scale_color_manual(values=c("#c0d2f0", "#f28095")) +
+    theme_minimal() +
+    geom_text_repel() +
+    ggtitle(sprintf("MA plot, Sample: %s vs. %s", condition1, condition2)) +
+    theme_bw() + 
+    theme(plot.title = element_text(hjust=0.5, face="bold", size = 12))
+  ggsave(plot = ma.plot, filename = "MA_plot.svg", path = path.to.save.output, dpi = 300, width = 14, height = 10)
+  
+  ##### heatmap
+  sig.genes.with.highlogFC.up <- subset(input.df, (input.df$sig == "Sig. genes") & (input.df$log2FoldChange > 0)) %>% arrange(desc(log2FoldChange)) %>% head(20)
+  sig.genes.with.highlogFC.down <- subset(input.df, (input.df$sig == "Sig. genes") & (input.df$log2FoldChange < 0)) %>% arrange(desc(log2FoldChange)) %>% tail(20)
+  sig.genes.with.highlogFC <- rbind(sig.genes.with.highlogFC.up, sig.genes.with.highlogFC.down)
+  
+  input.to.heatmap <- subset(sig.genes.with.highlogFC, select = c("gene_id", "gene_name", filtered.metadata$sample))
+  if (nrow(input.to.heatmap) > 0){
+    heatmap.values <- log10(input.to.heatmap[,3:(dim(input.to.heatmap)[2])] + 1)
+    selected.genes.heatmap.plot <- heatmaply(heatmap.values, 
+                                             main= sprintf("Heatmap, Sample: %s vs. %s", condition1, condition2),
+                                             method = "plotly",labRow=input.to.heatmap$gene_name,
+                                             xlab = "Samples", ylab = "Genes", width = 1000, height = 600,
+                                             showticklabels = c(TRUE, FALSE), show_dendrogram = c(FALSE, TRUE),
+                                             key.title = "log10 scale colormap",
+                                             label_names = c("Gene", "Sample", "Expression"),
+                                             k_col = 2, file = file.path(path.to.save.output, "heatmap.html"))
+  }
+  saveRDS(deseq.output, file.path(path.to.save.output, "deseq_output.rds"))
+  return(deseq.output)
 }
